99c4c5e9844ec028c1c3b47e8005ad9d05058196
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-21 14:44:24 +0200

    Bump development status

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email='harm.van.zeeland@gmail.com',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
